7319fcb5858709716cb5b669b94acc1c0637a2fe
Ruslan Filistovich <ruslan.filistovich@intexsoft.by>
2019-12-23 15:23:23 +0300

    Initial Commit

---
+++ test-fastlane-swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test-fastlane-swift
+//
+//  Created by Ruslan Filistovich on 23/12/2019.
+//  Copyright © 2019 Ruslan Filistovich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
